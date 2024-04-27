6072b9c1ea5ceab00da8f44bd1ab7a090d92d6fc
Adam <timmlion.istiris@gmail.com>
2024-04-27 09:35:55 +0200

    Db update

---
+++ SmartDj.Server/Migrations/20240427073328_modelRefactor.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SmartDj.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class modelRefactor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Played",
+                table: "SongRequests",
+                newName: "WasPlayed");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WasPlayed",
+                table: "SongRequests",
+                newName: "Played");
+        }
+    }
+}
