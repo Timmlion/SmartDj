6c4552bfb4c8ec19cbc116783ff7d1a21fa515d0
Adam <timmlion.istiris@gmail.com>
2024-04-27 10:12:28 +0200

    moved data source to service

--- SmartDj.Gui/Components/SongRequestGrid.razor.cs
+++ SmartDj.Gui/Components/SongRequestGrid.razor.cs
@@ -1,14 +1,19 @@
 using BlazorBootstrap;
 using Microsoft.AspNetCore.Components;
+using SmartDj.Gui.Services;
 using SmartDj.Shared.Models;
 
 namespace SmartDj.Gui.Components;
 
 public partial class SongRequestGrid : ComponentBase
 {
+    [Inject]
+    private SongRequestService _songRequestService { get; set; }
+    
     BlazorBootstrap.Grid<SongRequest> grid = default!;
     private IEnumerable<SongRequest> songRequests = default!;
 
+    
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         await base.OnAfterRenderAsync(firstRender);
@@ -17,19 +22,10 @@ public partial class SongRequestGrid : ComponentBase
     private async Task<GridDataProviderResult<SongRequest>> SongRequestDataProvider(GridDataProviderRequest<SongRequest> request)
     {
         if (songRequests is null) // pull employees only one time for client-side filtering, sorting, and paging
-            songRequests = GetSongRequests(); // call a service or an API to pull the employees
+            songRequests = _songRequestService.GetSongRequests(); // call a service or an API to pull the employees
 
         return await Task.FromResult(request.ApplyTo(songRequests));
     }
 
-    private IEnumerable<SongRequest> GetSongRequests()
-    {
-        return new List<SongRequest>
-        {
-            new SongRequest{Id = 1, DateCreated = DateTime.Now, RequestorName = "Zuzia", DedicateeName = "Zbigniew", SongTitle = "Tytuł piosenki", Message = "Dedykacja, Lorem ipsum pipsum sripsum tripsum bombiksum fafarafa fifirafifo", WasPlayed = true},
-            new SongRequest{Id = 2, DateCreated = DateTime.Now, RequestorName = "Zuzia2", DedicateeName = "Zbigniew", SongTitle = "Tytuł piosenki", Message = "Dedykacja, Lorem ipsum pipsum sripsum tripsum bombiksum fafarafa fifirafifo", WasPlayed = false},
-            new SongRequest{Id = 3, DateCreated = DateTime.Now, RequestorName = "Zuzia3", DedicateeName = "Zbigniew", SongTitle = "Tytuł piosenki", Message = "Dedykacja, Lorem ipsum pipsum sripsum tripsum bombiksum fafarafa fifirafifo", WasPlayed = false},
-            new SongRequest{Id = 4, DateCreated = DateTime.Now, RequestorName = "Zuzia4", DedicateeName = "Zbigniew", SongTitle = "Tytuł piosenki", Message = "Dedykacja, Lorem ipsum pipsum sripsum tripsum bombiksum fafarafa fifirafifo", WasPlayed = false},
-        };
-    }
+    
 }
--- SmartDj.Gui/Services/SongRequestService.cs
+++ SmartDj.Gui/Services/SongRequestService.cs
@@ -1,6 +1,17 @@
+using SmartDj.Shared.Models;
+
 namespace SmartDj.Gui.Services;
 
 public class SongRequestService
 {
-    
+    public IEnumerable<SongRequest>? GetSongRequests()
+    {
+        return new List<SongRequest>
+        {
+            new SongRequest{Id = 1, DateCreated = DateTime.Now, RequestorName = "Zuzia", DedicateeName = "Zbigniew", SongTitle = "Tytuł piosenki", Message = "Dedykacja, Lorem ipsum pipsum sripsum tripsum bombiksum fafarafa fifirafifo", WasPlayed = true},
+            new SongRequest{Id = 2, DateCreated = DateTime.Now, RequestorName = "Zuzia2", DedicateeName = "Zbigniew", SongTitle = "Tytuł piosenki", Message = "Dedykacja, Lorem ipsum pipsum sripsum tripsum bombiksum fafarafa fifirafifo", WasPlayed = false},
+            new SongRequest{Id = 3, DateCreated = DateTime.Now, RequestorName = "Zuzia3", DedicateeName = "Zbigniew", SongTitle = "Tytuł piosenki", Message = "Dedykacja, Lorem ipsum pipsum sripsum tripsum bombiksum fafarafa fifirafifo", WasPlayed = false},
+            new SongRequest{Id = 4, DateCreated = DateTime.Now, RequestorName = "Zuzia4", DedicateeName = "Zbigniew", SongTitle = "Tytuł piosenki", Message = "Dedykacja, Lorem ipsum pipsum sripsum tripsum bombiksum fafarafa fifirafifo", WasPlayed = false},
+        };
+    }
 }
