08a7788fedcaf4d05478b4e3db58312713a4faeb
Adam <timmlion.istiris@gmail.com>
2024-05-13 18:09:20 +0200

    refactor

--- SmartDj.Proxy/Data/DataContext.cs
+++ SmartDj.Proxy/Data/DataContext.cs
@@ -7,5 +7,5 @@ public class DataContext : DbContext
 {
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
-    public DbSet<User> SongRequests { get; set; }
+    public DbSet<User> Users { get; set; }
 }
