b7fc08d7a9293a871e6746dfc9f2dee8419567ee
Adam <timmlion.istiris@gmail.com>
2024-05-17 17:49:07 +0200

    cleanup

--- SmartDj.Proxy/Models/User.cs
+++
@@ -1,8 +0,0 @@
-namespace SmartDj.Proxy.Models;
-
-public class User
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Token { get; set; }
-}
--- SmartDj.Proxy/Services/ProxyService.cs
+++
@@ -1,9 +0,0 @@
-namespace SmartDj.Proxy.Services;
-
-public class ProxyService
-{
-    public bool CheckId(string id)
-    {
-        throw new NotImplementedException();
-    }
-}
